918eae596c61c63a6247f4b1aa83ee472b8178ba
Igor Novák <igincan@Igincan.local>
2019-10-31 11:18:18 +0100

    added more color; increased speed; bug fixed

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var ctx = document.getElementById('ctx').getContext('2d');
+var canvas = document.getElementById('ctx');
+
+var ctx = canvas.getContext('2d');
 
 var objects = [];
 
-var k = prompt('How many groups? (max 9)');
+var groups = ['red', 'green', 'blue', 'brown', 'yellow', 'purple', 'pink', 'orange', 'grey', 'cyan', 'darkgoldenrod', 'darkolivegreen', 'darkslateblue', 'lightgreen', 'coral'];
+
+var k = prompt('How many groups? (max ' + groups.length + ')');
 
 var numberOfObjects = 30000;
 
-var delayInAnimation = 1000;
+var delayInAnimation = 500;
 
 var spacePressed = false;
-
-var groups = ['red', 'green', 'blue', 'brown', 'yellow', 'purple', 'pink', 'orange', 'grey'];
 
 var centroids = [];
 for (let i = 0; i < k; i++) {
@@ -26,16 +28,16 @@ for (let i = 0; i < k; i++) {
     });
 }
 
-var nothingHasChanged = false;
+var nothingHasChanged = 0;
 
-var drawAllObjects = function() {
+var drawObjects = function() {
     objects.forEach(object => {
         ctx.fillStyle = groups[object.group];
         ctx.fillRect(object.x - 2, object.y - 2, 5, 5);
     });
 };
 
-var createRandomObjects = function() {
+var generateRandomObjects = function() {
     for (let i = 0; i < numberOfObjects; i++) {
         objects.push({
             x: Math.random() * 500,
@@ -45,11 +47,11 @@ var createRandomObjects = function() {
     }
 };
 
-var getDistanceBetweenObjectAndCentroid = function(object, centroid) {
-    return Math.sqrt((object.x - centroid.x)*(object.x - centroid.x) + (object.y - centroid.y)*(object.y - centroid.y));
+var getDistance = function(x1, y1, x2, y2) {
+    return Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
 };
 
-var createRandomGroups = function() {
+var generateRandomGroups = function() {
     for (let i = 0; i < objects.length; i++) {
         objects[i].group = Math.floor(Math.random() * k);
         centroids[objects[i].group].members.size++;
@@ -60,12 +62,17 @@ var createRandomGroups = function() {
 
 var calculateCentroids = function() {
     for (let i = 0; i < k; i++) {
+        if (centroids[i].members.size === 0) {
+            continue;
+        }
         let oldX = centroids[i].x;
         let oldY = centroids[i].y;
         centroids[i].x = centroids[i].members.sumOfXs / centroids[i].members.size;
         centroids[i].y = centroids[i].members.sumOfYs / centroids[i].members.size;
         if (oldX === centroids[i].x && oldY === centroids[i].y) {
-            nothingHasChanged = true;
+            nothingHasChanged++;
+        } else {
+            nothingHasChanged = 0;
         }
     }
 };
@@ -88,18 +95,18 @@ var assignAllObjectsToClosestCentroid = function() {
         centroids[i].members.sumOfYs = 0;
     }
     for (let i = 0; i < objects.length; i++) {
-        objects[i].group = getIndexOfClosestCentroid(objects[i]);
+        objects[i].group = getIndexOfClosestCentroid(objects[i].x, objects[i].y);
         centroids[objects[i].group].members.size++;
         centroids[objects[i].group].members.sumOfXs += objects[i].x;
         centroids[objects[i].group].members.sumOfYs += objects[i].y;
     }
 };
 
-var getIndexOfClosestCentroid = function(object) {
+var getIndexOfClosestCentroid = function(x, y) {
     let closestDistance = 9999999;
     let closestIndex;
     for (let i = 0; i < k; i++) {
-        let distance = getDistanceBetweenObjectAndCentroid(object, centroids[i]);
+        let distance = getDistance(x, y, centroids[i].x, centroids[i].y);
         if (distance < closestDistance) {
             closestDistance = distance;
             closestIndex = i;
@@ -113,22 +120,35 @@ var clearScreen = function() {
     ctx.fillRect(0, 0, 500, 500);
 };
 
+getPreciseMousePostition = function(x, y) {
+    return {
+        x: x - canvas.getBoundingClientRect().left + 1,
+        y: y - canvas.getBoundingClientRect().top + 1
+    };
+};
+
+document.onclick = function(event) {
+    let mousePosition = getPreciseMousePostition(event.clientX, event.clientY);
+    console.log(centroids[getIndexOfClosestCentroid(mousePosition.x, mousePosition.y)]);
+};
+
 var loop = function(multiplicator) {
     assignAllObjectsToClosestCentroid();
     clearScreen();
-    drawAllObjects();
+    drawObjects();
     drawCentroids();
     setTimeout(function() {
         calculateCentroids();
         clearScreen();
-        drawAllObjects();
+        drawObjects();
         drawCentroids();
         setTimeout(function() {
-            if (!nothingHasChanged) {
+            if (nothingHasChanged < 2) {// if (!nothingHasChanged) {
                 loop(multiplicator - 1);
             } else {
-                console.log('Finish!');
+                console.log('Done!');
                 console.log('Groups:', centroids);
+                alert('Done!')
             }
         }, delayInAnimation * (multiplicator > 1 ? 2 : (multiplicator < -3 ? 0.2 : 1)));
     }, delayInAnimation * (multiplicator > 1 ? 2 : (multiplicator < -3 ? 0.2 : 1)));
@@ -136,13 +156,13 @@ var loop = function(multiplicator) {
 
 console.log('Start!');
 setTimeout(function() {
-    createRandomObjects();
-    drawAllObjects();
+    generateRandomObjects();
+    drawObjects();
     setTimeout(function() {
-        createRandomGroups();
+        generateRandomGroups();
         console.log('Groups:', JSON.parse(JSON.stringify(centroids)));
         clearScreen();
-        drawAllObjects();
+        drawObjects();
         setTimeout(function() {
             calculateCentroids();
             drawCentroids();
